2e845c8f5a4aca0288fe7aa8b258f6c19d8e14aa
paigeharton <71464175+paigeharton@users.noreply.github.com>
2020-11-30 16:22:00 -0500

    Update Final_Project_Clean_Data.R

--- R_Code/Final_Project_Clean_Data.R
+++ R_Code/Final_Project_Clean_Data.R
@@ -2,10 +2,8 @@
 
 library(janitor)
 
-pth <- read.csv(file='Raw_Data/surveydata.csv, na.strings=c("", "NA"))
+surveydata <- read.csv(file='Raw_Data/surveydata.csv, na.strings=c("", "NA")) %>% clean_names()
 
-surveydata <- read.xlsx(pth, sheet = 'Expat_307') %>% clean_names()
-
-surveydata$teachingcountry <- recode(surveydata$teaching_at, '1' = "Indonesia", '2' = "Thailand", '3' = "Philippines", '4' = "Vietnam", '5' = "Singapore")
+surveydata$teachingcountry <- dplyr::recode(surveydata$teaching_at, '1' = "Indonesia", '2' = "Thailand", '3' = "Philippines", '4' = "Vietnam", '5' = "Singapore")
 
 write.csv(x=surveydata, file = "data.csv")
