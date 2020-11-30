90255d01b25141f1e8fd44638dd885dd5602b750
paigeharton <71464175+paigeharton@users.noreply.github.com>
2020-11-30 15:57:10 -0500

    Update InstallPackages.R

--- R_Code/InstallPackages.R
+++ R_Code/InstallPackages.R
@@ -2,12 +2,11 @@
 
 #install packages
 
-install.packages('janitor')
-install.packages('epiDisplay')
-install.packages('ggplot2')
-install.packages('kableExtra')
-install.packages('dplyr')
-install.packages('tidyverse')
+install.packages('janitor', repos = 'http://cran.us.r-project.org')
+install.packages('epiDisplay', repos = 'http://cran.us.r-project.org')
+install.packages('ggplot2', repos = 'http://cran.us.r-project.org')
+install.packages('kableExtra', repos = 'http://cran.us.r-project.org')
+install.packages('dplyr', repos = 'http://cran.us.r-project.org')
+install.packages('tidyverse', repos = 'http://cran.us.r-project.org')
 
 #end
-
